905ec89ad289953f508ec0ef4d56e3ba579a25a8
lucianovsjr <lucianovsjr@gmail.com>
2020-01-17 20:07:19 -0300

    Aula 26 Funções

---
+++ exercicios-js/fudamentos/funcaoEmQuaseTudo.js
@@ -0,0 +1,9 @@
+/*
+    Função é o papel mais importante do JS, papel central.
+    JS é centrado em funções.
+*/
+console.log(typeof Object);
+
+//internamente é uma função
+class Produto {}
+console.log(typeof Produto)
