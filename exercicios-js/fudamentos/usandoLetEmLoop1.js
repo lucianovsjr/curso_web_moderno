bdb2a44c379953b277b37d7ab9e87baa7ea5f5b2
lucianovsjr <lucianovsjr@gmail.com>
2020-01-18 18:22:47 -0300

    Aula 33 let loop

---
+++ exercicios-js/fudamentos/usandoLetEmLoop1.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 10;i++) {
+    console.log(i);
+}
+// i so estara disponivel no bloco do for
+console.log('i =', i);// ReferenceError: i is not defined
---
+++ exercicios-js/fudamentos/usandoVarEmLoop1.js
@@ -0,0 +1,5 @@
+for (var i = 0; i < 10;i++) {
+    console.log(i);
+}
+//i ficara com valor que fez ele sair do laco
+console.log('i =', i);
