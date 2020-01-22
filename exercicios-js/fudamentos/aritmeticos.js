9daaf71e71b1e6c832e8780f7b1aeaadf633524d
lucianovsjr <lucianovsjr@gmail.com>
2020-01-22 20:58:48 -0300

    Aula 45 Operadores Artiméticos

---
+++ exercicios-js/fudamentos/aritmeticos.js
@@ -0,0 +1,17 @@
+/*
+    Operadores binarios. Dois operando.
+*/
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const soma = a + b + c + d;
+console.log(soma);
+
+const subtracao = d - b;
+const multiplicacao = a * b;
+const divisao = d / a;
+const modulo = a % 2; // utilziado para saber e par ou impa
+
+console.log(subtracao, multiplicacao, divisao, modulo);
+
+// simbolo de menos e um operador unario
+console.log(-subtracao);
