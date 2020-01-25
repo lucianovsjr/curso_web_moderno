1df66b856127134943654c425bf0d5f76fdd649e
lucianovsjr <lucianovsjr@gmail.com>
2020-01-25 18:36:43 -0300

    Aula 57 Switch

---
+++ exercicios-js/controle/switch.js
@@ -0,0 +1,34 @@
+// switch e uma selecao multipla
+
+const imprimirResultado = function (nota) {
+    /*
+        Entra no case e vai executando o codigo,
+        inclusive dos cases abaixo. Uso do break
+        para parar a execucao.
+    */
+    switch (Math.floor(nota)) {
+        // 10 ou 9 -> imprimi 'Quadro de Honra'
+        case 10:
+        case 9:
+            console.log('Quadro de Honra');
+            break;
+        case 8: case 7:
+            console.log('Aprovado');
+            break;
+        case 6: case 5: case 4:
+            console.log('Recuperação');
+            break;
+        case 3: case 2: case 1:
+            console.log('Reprovado');
+            break;
+        default:
+            console.log('Nota inválida');
+    }
+}
+
+imprimirResultado(10);
+imprimirResultado(8.9);
+imprimirResultado(6.55);
+imprimirResultado(2.3);
+imprimirResultado(-1);
+imprimirResultado(11);
