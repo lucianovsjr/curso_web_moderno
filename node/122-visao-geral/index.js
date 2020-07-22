22c4cc5576aeda4aac084dabff60edf581cf0ac1
lucianovsjr <lucianovsjr@gmail.com>
2020-07-22 20:19:39 -0300

    Aula 122

---
+++ node/122-visao-geral/index.js
@@ -0,0 +1,8 @@
+/*
+  * Node é um runtime que interpreta JS (Utiliza V8);
+
+  * Quando utilizamos Node a ideia é montar serviços web,
+  não trabalhar service side igual ao php, python e java;
+
+  * É um ambiente de execução no lado do servidor;
+*/
